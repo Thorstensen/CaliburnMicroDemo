25920bad05e958b932a4d2653aa9490ff67a20cd
Tobias Moe Thorstensen <tobias.thorstensen@gmail.com>
2018-11-03 14:49:54 +0100

    Solution starts up with autofac container

---
+++ src/Caliburn.Micro.Demo.Host/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using Caliburn.Micro.Autofac;
+using Caliburn.Micro.Demo.Host.ViewModels;
+
+namespace Caliburn.Micro.Demo.Host
+{
+    public class Bootstrapper : AutofacBootstrapper<ShellViewModel>
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            base.OnStartup(sender, e);
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ src/Caliburn.Micro.Demo.Host/ViewModels/ShellViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Caliburn.Micro.Demo.Contracts;
+using System.ComponentModel.Composition;
+
+namespace Caliburn.Micro.Demo.Host.ViewModels
+{
+    [Export(typeof(IShell))]
+    public class ShellViewModel : PropertyChangedBase, IShell
+    {
+    }
+}
