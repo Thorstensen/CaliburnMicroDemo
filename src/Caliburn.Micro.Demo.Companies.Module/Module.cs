daeabef7d57b7d6f6b5e86b7853bd2f1a39c4db9
Tobias Moe Thorstensen <tobias.thorstensen@gmail.com>
2018-11-04 12:11:52 +0100

    removed unused code

--- src/Caliburn.Micro.Demo.Companies.Module/Module.cs
+++ src/Caliburn.Micro.Demo.Companies.Module/Module.cs
@@ -1,13 +1,7 @@
 ﻿using Autofac;
 using Caliburn.Micro.Demo.Companies.Module.ViewModels;
-using Caliburn.Micro.Demo.Companies.Module.Views;
 using Caliburn.Micro.Demo.Contracts;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.Composition;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Caliburn.Micro.Demo.Companies.Module
 {
