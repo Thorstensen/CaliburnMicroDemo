220c881b1b778d2511a7091300e9f1d18af62bc1
Tobias Moe Thorstensen <tobias.thorstensen@gmail.com>
2018-11-05 17:14:24 +0100

    added apple store

---
+++ src/Caliburn.Micro.Demo.Shopping.Apple.Module/AppleModule.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using Caliburn.Micro.Demo.Contracts;
+using Caliburn.Micro.Demo.Shopping.Apple.Module.Model;
+using Caliburn.Micro.Demo.Shopping.Extensions;
+using System.ComponentModel.Composition;
+
+namespace Caliburn.Micro.Demo.Shopping.Apple.Module
+{
+    [Export(typeof(IModule))]
+    public class AppleModule : IModule
+    {
+        public void RegisterComponents(ContainerBuilder builder)
+        {
+            builder.RegisterStore<AppleStore>();
+        }
+    }
+}
